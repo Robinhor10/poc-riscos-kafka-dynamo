c5ddc8960832493493e777c85755b1f5f6797a14
Robinho <robinhopj@gmail.com>
2023-04-15 16:43:32 -0300

    criando testes e consumindo mensagens

--- src/main/java/org/acme/kafka/ConsumedRiscosResource.java
+++ src/main/java/org/acme/kafka/ConsumedRiscosResource.java
@@ -6,8 +6,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.acme.kafka.dto.RiscoRequestDto;
 import org.acme.kafka.quarkus.Risco;
 import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestSseElementType;
 
 import io.smallrye.mutiny.Multi;
@@ -15,6 +17,7 @@ import io.smallrye.mutiny.Multi;
 @ApplicationScoped
 @Path("/consumed-riscos")
 public class ConsumedRiscosResource {
+    private static final Logger LOGGER = Logger.getLogger(ConsumedRiscosResource.class);
 
     @Channel("riscos-from-kafka")
     Multi<Risco> riscos;
@@ -23,6 +26,7 @@ public class ConsumedRiscosResource {
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @RestSseElementType(MediaType.TEXT_PLAIN)
     public Multi<String> stream() {
-        return riscos.map(risco -> String.format("'%s' from %s", risco.getCadastro(), risco.getValor()));
+        LOGGER.infof("Consumindo mensagem do kafka");
+        return riscos.map(RiscoRequestDto -> String.format("%s %s %s %s", RiscoRequestDto.getCadastro(), RiscoRequestDto.getDocumento(),RiscoRequestDto.getProduto(), RiscoRequestDto.getValor()));
     }
 }
